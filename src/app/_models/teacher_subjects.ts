879fa09705dc6b3765fba194b02eb74a228bfc01
semajame <jamesanquillano@gmail.com>
2024-08-26 11:52:27 +0800

    added view of schedules of teachers

---
+++ src/app/_models/teacher_subjects.ts
@@ -0,0 +1,12 @@
+export interface TeacherSubject {
+  id: number;
+  teacherId: number;
+  teacherName: string;
+  subject_code: string;
+  subject: string;
+  units: number;
+  room: string;
+  start: string;
+  end: string;
+  day: string;
+}
